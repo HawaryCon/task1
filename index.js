27bc2504c8e03ddad45d7981f04ba80963033df8
HawaryCon <ahmed.hwary38@gmail.com>
2023-03-03 18:39:50 +0400

    eshta8alll

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import productsRoutes from './productsR.js';
 import { config } from 'dotenv';
+import mongodb from 'mongodb';
+import CreateProductTag from './productTag.js';
 config();
 
 
@@ -17,6 +19,17 @@ app.use('/products', productsRoutes);
 app.get("/", (req,res) => {
     return res.send("hello world");
 })
+
+app.delete("/:id", async (req,res) => {
+    const {id} = req.params;
+    console.log(id);
+    const result = await CreateProductTag.deleteOne({ _id: new mongodb.ObjectId(req.params.id)})
+    // const data = await ();
+    // const result = await data.deleteOne(})
+    res.status(!!result.deletedCount? 200:400).json(result);
+    
+})
+
 
 
 
--- productsR.js
+++ productsR.js
@@ -1,6 +1,7 @@
 import  express  from 'express';
-import { getProducts , createProduct } from './productsC.js';
+import { getProducts , createProduct  } from './productsC.js';
 const router = express.Router();
 router.get('/' , getProducts );
 router.post('/create' , createProduct );
+
 export default router;
